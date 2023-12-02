dc845f1178195ca17334a5faaacadc6a7af74249
jmadupalli <jayanth.madupalli@gmail.com>
2023-12-02 14:08:58 -0600

    Initial commit

---
+++ src/main/java/com/jay/springkeeptrack/SpringKeepTrackApplication.java
@@ -0,0 +1,13 @@
+package com.jay.springkeeptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeepTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeepTrackApplication.class, args);
+    }
+
+}
